337cf9baacf6b894a4a307edd63177a4ee67439c
Jorge Alonso Podadera <yoyi1239707@gmail.com>
2021-09-08 09:41:20 -0400

    añadida seguridad a rutas

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -12,8 +12,8 @@ const Administrador = auth.Adminitrador;
 //routes
 router.get('/', [verifyToken], userCtr.getUser);
 router.get('/:id', [verifyToken, Administrador], userCtr.getUserById);
-router.get('/paginated/:page', [verifyToken, Administrador], userCtr.getUserPaginated);
-router.put('/:id', [verifyToken], userCtr.editUser);
+router.get('/paginated/:page', [verifyToken], userCtr.getUserPaginated);
+router.put('/:id', [verifyToken, Administrador], userCtr.editUser);
 router.delete('/:id', [verifyToken, Administrador], userCtr.deleteUser);
 
 module.exports = router;
